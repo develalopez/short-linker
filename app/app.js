b73d94dec97951b1122c32414f7ba136d2f52e05
Juan David Murillo <juancho20sp@gmail.com>
2022-07-08 19:53:27 -0500

    CodeQL fix implemented

--- app/app.js
+++ app/app.js
@@ -1,8 +1,18 @@
 const express = require('express')
+const rateLimit = require('express-rate-limit');
 const path = require('path');
 
 const app = express()
 const port = process.env.PORT || 8080;
+
+// Set up rate limiter: maximum of five requests per minute
+const limiter = rateLimit({
+  windowMs: 1*60*1000, // 1 minute
+  max: 5
+});
+
+// Apply rate limiter to all requests
+app.use(limiter);
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, './src/index.html'));
